3b5d9063912a7488453aefe846e7019e4e2f2bb7
unknown <antoniohenrique3180@gmail.com>
2024-06-07 21:07:36 -0300

    segundo commit

---
+++ projeto2/Variaveis.py
@@ -0,0 +1,10 @@
+caixadenumeros = "cavalo"
+caixadenumeros2 = 2
+
+caixa = [caixadenumeros,caixadenumeros2]
+print([caixa,caixa,caixa,caixa])
+
+if(caixadenumeros!=caixadenumeros2):
+    print("cavalo")
+else:
+    print("iguana")
